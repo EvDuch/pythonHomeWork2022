6be77c3711d01fe7d11b379af924b08514f8edcb
EvDuch <79639567+EvDuch@users.noreply.github.com>
2022-03-23 01:49:15 +0700

    Add files via upload

---
+++ day2/part1.py
@@ -0,0 +1,18 @@
+with open("input.TXT") as f:
+	lines = f.readlines()          #СЧИТЫВАЕМ ВСЕ СТРОКИ ДОКУМЕНТА В СПИСКИ
+	f.seek(0)					   #ВОЗВРАЩЕМ КУРСИВ В САМОЕ НАЧАЛО
+	res = 0
+	for i in range(0, len(lines)): #ОБРАЩАЕМСЯ К КАЖДОЙ СТРОКЕ ЧЕРЕЗ ЦИКЛ "for"
+		
+		#ДОСТАЁМ ИЗ КАЖДОЙ СТРОКИ ТОЛЬКО ЧИСЛА
+		line = f.readline()			#ЧИТАЕМ ПЕРВУЮ СТРОКУ
+		lineList = line.replace("\n", "").split("x")  #РАЗДЕЛЯЕМ ЭТУ СТРОКУ В СПИСОК ТАК, ЧТО БЫ ОСТАЛИСТЬ ОДНИ ЦИФРЫ ((И УБИРАЕМ /n) replace(<что заменяем>,<на что заменяем>))
+		lineList = list(map(int,lineList))
+		lineList = sorted(lineList)   #УПОРЯДОЧИВАЕТ ЧИСЛА В ПОРЯДКЕ ВОЗРАСТАНИЯ В СПИСКЕ
+		res += 2*lineList[0]*lineList[1]+2*lineList[0]*lineList[2]+2*lineList[1]*lineList[2]+lineList[0]*lineList[1]	
+print(res)
+
+newfile = open("output.TXT", "w")
+d = str(res)
+newfile.write(d) 
+newfile.close()
