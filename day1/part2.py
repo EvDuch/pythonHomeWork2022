8bf3e57a920eabc1a6d46c818830b73ff29234e3
EvDuch <79639567+EvDuch@users.noreply.github.com>
2022-03-23 01:36:24 +0700

    Update part2.py

--- day1/part2.py
+++ day1/part2.py
@@ -1,15 +1,21 @@
 f = open("input.TXT", 'r')
 List = f.readline() 
+op = 0
+cl = 0
 cnt = 0
-
 for i in range(0, len(List)):
-	if (List[i] == '(') or (List[i] == ')'):
-		cnt = cnt + 1
-
-print("Позиция персонажа: " + cnt)
+	cnt+=1
+	if List[i] == '(':
+		op += 1
+	if List[i] == ')':
+		cl += 1
+	if op - cl == -1:
+		break
+print("Ответ: " + str(cnt))
 f.close()
 
 newfile = open("output.TXT", "w")
 d = str(cnt)
 newfile.write(d) 
 newfile.close()
+
